aefe0677f949514c83d7f56c65e2eb48e96a065a
rankun203 <rankun203@gmail.com>
2015-12-05 17:37:21 +0800

    UPDATE: update command

--- commands.js
+++ commands.js
@@ -4,7 +4,7 @@ var commands = [
     desc: 'Update Simple Accounting Frontend Website',
     test: 'refs/heads/dev',
     timeout: 1000 * 60 * 10,
-    command: 'cd /home/rankun203/git/mine/outsourcing/simple-accounting/simple-accounting-web && . ./bin/updateWebsite.sh'
+    command: 'cd /alidata/docker/git/simple-accounting-web && . ./bin/updateWebsite.sh'
   },
   {
     id: 'helloWorld',
