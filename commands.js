2631d4cb36022924285c96af0d44c22c252256de
rankun203 <rankun203@gmail.com>
2015-12-05 16:49:04 +0800

    UPDATE: update command

--- commands.js
+++ commands.js
@@ -3,7 +3,7 @@ var commands = [
     id: 'updateSAWeb',
     desc: 'Update Simple Accounting Frontend Website',
     test: 'refs/heads/dev',
-    command: 'cd /home/rankun203/git/mine/outsourcing/simple-accounting/simple-accounting-web && git checkout dev && git pull'
+    command: 'cd /home/rankun203/git/mine/outsourcing/simple-accounting/simple-accounting-web && git pull origin dev && git checkout dev'
   },
   {
     id: 'helloWorld',
